2fe8395e25a3d536120b0a678b766be80e7eb010
Ethan <ethanmontera@gmail.com>
2023-11-07 23:15:11 +0800

    [Ethan][Changed everything back to normal]

--- app.js
+++ app.js
@@ -1,9 +1,2 @@
 document.querySelector('.menu-btn').addEventListener('click', () => document.querySelector('.main-menu').classList.toggle('show'));
 
-$( '.js-input' ).keyup(function() {
-    if( $(this).val() ) {
-       $(this).addClass('not-empty');
-    } else {
-       $(this).removeClass('not-empty');
-    }
-  });
