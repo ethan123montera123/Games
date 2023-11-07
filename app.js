160472be5249c2dcf2147217d934c74f6a226437
Ethan <ethanmontera@gmail.com>
2023-11-07 23:14:04 +0800

    [Ethan][Changes in landing page]

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 document.querySelector('.menu-btn').addEventListener('click', () => document.querySelector('.main-menu').classList.toggle('show'));
+
+$( '.js-input' ).keyup(function() {
+    if( $(this).val() ) {
+       $(this).addClass('not-empty');
+    } else {
+       $(this).removeClass('not-empty');
+    }
+  });
