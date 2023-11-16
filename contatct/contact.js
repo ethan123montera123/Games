7aa1cbce33ed2510004530c2472f99129e4ebe33
Ethan <ethanmontera@gmail.com>
2023-11-16 22:52:18 +0800

    [Ethan][Changes in backend contact]

--- contatct/contact.js
+++ contatct/contact.js
@@ -1,9 +1,6 @@
 function sendMail() {
     var params = {
-        first: document.getElementById("first").value,
-        email: document.getElementById("email").value,
-        last: document.getElementById("last").value,
-        message: document.getElementById("message").value,
+        name: document.getElementById("email").value,
     };
 
     const serviceID = "service_9i6b9do";
@@ -12,49 +9,16 @@ function sendMail() {
     emailjs
         .send(serviceID, templateID, params)
         .then((res) => {
-            document.getElementById("first").value = "";
-            document.getElementById("last").value = "";
             document.getElementById("email").value = "";
-            document.getElementById("message").value = "";
 
             console.log(res);
             alert("Your Message was Sent Successfully");
         })
 
     .catch((err) => console.log(err));
-
 }
 
-const btn = document.getElementById('button');
-
-document.getElementById('form')
-    .addEventListener('submit', function(event) {
-        event.preventDefault();
-
-        btn.value = 'Sending...';
-
-        const serviceID = 'service_9i6b9do';
-        const templateID = 'template_ygywmxh';
-
-        emailjs.sendForm(serviceID, templateID, this)
-            .then(() => {
-                btn.value = 'Send Email';
-                alert('Sent!');
-            }, (err) => {
-                btn.value = 'Send Email';
-                alert(JSON.stringify(err));
-            });
-    });
-
-    document.querySelector('#contact-form').addEventListener('submit', (e) => {
-        e.preventDefault();
-        e.target.elements.first.value = '';
-        e.target.elements.last.value = '';
-        e.target.elements.email.value = '';
-        e.target.elements.message.value = '';
-    });
-
-    // Other important pens.
+// Other important pens.
 // Map: https://codepen.io/themustafaomar/pen/ZEGJeZq
 // Dashboard: https://codepen.io/themustafaomar/pen/jLMPKm
 
@@ -63,57 +27,57 @@ let dropdownIsOpen = false
 
 // Handle dropdown menues
 if (dropdowns.length) {
-  // Usually I don't recommend doing this (adding many event listeners to elements have the same handler)
-  // Instead use event delegation: https://javascript.info/event-delegation
-  // Why: https://gomakethings.com/why-event-delegation-is-a-better-way-to-listen-for-events-in-vanilla-js
-  // But since we only have two dropdowns, no problem with that. 
-  dropdowns.forEach((dropdown) => {
-    dropdown.addEventListener('click', (event) => {
-      let target = document.querySelector(`#${event.target.dataset.dropdown}`)
+    // Usually I don't recommend doing this (adding many event listeners to elements have the same handler)
+    // Instead use event delegation: https://javascript.info/event-delegation
+    // Why: https://gomakethings.com/why-event-delegation-is-a-better-way-to-listen-for-events-in-vanilla-js
+    // But since we only have two dropdowns, no problem with that. 
+    dropdowns.forEach((dropdown) => {
+        dropdown.addEventListener('click', (event) => {
+            let target = document.querySelector(`#${event.target.dataset.dropdown}`)
 
-      if (target) {
-        if (target.classList.contains('show')) {
-          target.classList.remove('show')
-          dropdownIsOpen = false
-        } else {
-          target.classList.add('show')
-          dropdownIsOpen = true
-        }
-      }
+            if (target) {
+                if (target.classList.contains('show')) {
+                    target.classList.remove('show')
+                    dropdownIsOpen = false
+                } else {
+                    target.classList.add('show')
+                    dropdownIsOpen = true
+                }
+            }
+        })
     })
-  })
 }
 
 // Handle closing dropdowns if a user clicked the body
 window.addEventListener('mouseup', (event) => {
-  if (dropdownIsOpen) {
-    dropdowns.forEach((dropdownButton) => {
-      let dropdown = document.querySelector(`#${dropdownButton.dataset.dropdown}`)
-      let targetIsDropdown = dropdown == event.target
+    if (dropdownIsOpen) {
+        dropdowns.forEach((dropdownButton) => {
+            let dropdown = document.querySelector(`#${dropdownButton.dataset.dropdown}`)
+            let targetIsDropdown = dropdown == event.target
 
-      if (dropdownButton == event.target) {
-        return
-      }
+            if (dropdownButton == event.target) {
+                return
+            }
 
-      if ((!targetIsDropdown) && (!dropdown.contains(event.target))) {
-        dropdown.classList.remove('show')
-      }
-    })
-  }
+            if ((!targetIsDropdown) && (!dropdown.contains(event.target))) {
+                dropdown.classList.remove('show')
+            }
+        })
+    }
 })
 
 // Open links in mobiles
 function handleSmallScreens() {
-  document.querySelector('.navbar-toggler')
-    .addEventListener('click', () => {
-      let navbarMenu = document.querySelector('.navbar-menu')
+    document.querySelector('.navbar-toggler')
+        .addEventListener('click', () => {
+            let navbarMenu = document.querySelector('.navbar-menu')
 
-      if (!navbarMenu.classList.contains('active')) {
-        navbarMenu.classList.add('active')
-      } else {
-        navbarMenu.classList.remove('active')
-      }
-    })
+            if (!navbarMenu.classList.contains('active')) {
+                navbarMenu.classList.add('active')
+            } else {
+                navbarMenu.classList.remove('active')
+            }
+        })
 }
 
 handleSmallScreens()
