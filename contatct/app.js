f607bd7dda9bc7a359a032a16c0dc39af833c0da
Ethan <ethanmontera@gmail.com>
2023-11-17 09:57:09 +0800

    [Ethan]

--- contatct/app.js
+++ contatct/app.js
@@ -12,7 +12,7 @@ document.getElementById('form')
         emailjs.sendForm(serviceID, templateID, this)
             .then(() => {
                 btn.value = 'Send Email';
-                alert('Sent!');
+                alert('Your Message was Sent Successfully. Please wait for a reply. Thank you!');
             }, (err) => {
                 btn.value = 'Send Email';
                 alert(JSON.stringify(err));
